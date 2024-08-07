5f61850797d247a5197f4cd59858f7e768e533d7
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-07 12:40:45 -0300

    Adicionado a DTO

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -1,13 +1,26 @@
 package com.example.cardapio.food;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+//@Getter                 //Gera os metodos getters dessa classe
+//@NoArgsConstructor      //Gera os contrustores dessa classe
+//@AllArgsConstructor     //Gera os contrustores dessa classe
+//@EqualsAndHashCode(of = "id")   //Indica que o id é representação unica da classe food
 public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
+    @Getter
     private String title;
+    @Getter
     private String image;
+    @Getter
     private Integer price;
+
 }
