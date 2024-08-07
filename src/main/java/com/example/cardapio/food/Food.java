4a00f4c4ef1f54f06f174d277fac2f520b9eeec8
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-07 13:39:21 -0300

    Finalizado o back-end

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -8,19 +8,20 @@ import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
-//@Getter                 //Gera os metodos getters dessa classe
-//@NoArgsConstructor      //Gera os contrutores dessa classe
-//@AllArgsConstructor     //Gera os contrutores dessa classe
-//@EqualsAndHashCode(of = "id")   //Indica que o id é representação unica da classe food
+@Getter                 //Gera os metodos getters dessa classe
+@NoArgsConstructor      //Gera os contrutores dessa classe
+@AllArgsConstructor     //Gera os contrutores dessa classe
+@EqualsAndHashCode(of = "id")   //Indica que o id é representação unica da classe food
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
     private Long id;
-    @Getter
     private String title;
-    @Getter
     private String image;
-    @Getter
     private Integer price;
 
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
 }
